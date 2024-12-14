08a09cdc45e4de578d16c530279ea4cbe4a5773a
amrith-git-01 <142132572+amrith-git-01@users.noreply.github.com>
2024-12-14 13:48:38 +0530

    Add files via upload

---
+++ src/features/ui/Header.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import SearchOrder from "../order/SearchOrder";
+import Username from "../user/Username";
+
+function Header() {
+	return (
+		<header className="flex items-center justify-between px-4 py-3 uppercase bg-yellow-400 border-b border-stone-200 sm:px-6 ">
+			<Link to="/" className="tracking-widest">
+				Fast React Pizza Co.
+			</Link>
+			<SearchOrder />
+			<Username />
+		</header>
+	);
+}
+
+export default Header;
